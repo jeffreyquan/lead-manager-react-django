2aad18d3487530bc35d1c8531a9fc76865b0cc8f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-06-07 01:43:22 +1000

    implemented Django token authentication

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { createMessage } from "./messages";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 export const getLeads = () => (dispatch) => {
   axios
@@ -11,7 +11,9 @@ export const getLeads = () => (dispatch) => {
         payload: res.data,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 export const deleteLead = (id) => (dispatch) => {
@@ -45,15 +47,7 @@ export const addLead = (lead) => (dispatch) => {
         payload: res.data,
       });
     })
-    .catch((err) => {
-      const errors = {
-        message: err.response.data,
-        status: err.response.status,
-      };
-
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors,
-      });
-    });
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
